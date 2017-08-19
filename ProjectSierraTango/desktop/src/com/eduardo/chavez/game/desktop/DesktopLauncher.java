1c9f3741c409afaa4d7a2937957f78e34a8a21c1
eduardo3150 <mccartney3150@gmail.com>
2017-08-19 10:03:03 -0600

    Projecto inicial

---
+++ ProjectSierraTango/desktop/src/com/eduardo/chavez/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eduardo.chavez.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eduardo.chavez.game.GameLoader;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameLoader(), config);
+	}
+}
