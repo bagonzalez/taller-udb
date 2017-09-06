b639fe6147eaaf7d49e802124087d9eb7ba175cf
eduardo3150 <mccartney3150@gmail.com>
2017-09-05 19:09:43 -0600

    Proyecto pingpong C#

---
+++ pingpongproject_ed/pingpongproject_ed/SettingsForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace pingpongproject_ed
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class SettingsForm : Gtk.Bin
+	{
+		public SettingsForm()
+		{
+			this.Build();
+		}
+	}
+}
