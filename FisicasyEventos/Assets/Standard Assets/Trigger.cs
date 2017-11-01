70727318c9c066afda85e1d497d16c174122359d
diaz2298 <omar2298@hotmail.com>
2017-11-01 15:34:52 -0600

    juego fisicas

---
+++ FisicasyEventos/Assets/Standard Assets/Trigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+
+	// Use this for initialization
+
+	void OnTriggerEnter(Collider other){
+
+
+		Debug.Log ("trigger enter");
+		if (gameObject.name == "destruir1") {
+		
+			Destroy (gameObject);
+		}
+		if (gameObject.name == "destruir2") {
+
+			Destroy (gameObject);
+		}
+
+	
+
+
+	}
+
+}
